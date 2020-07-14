dca5ec22ebdfe7a19c2d23f729ca66a4a67e2f43
ajpicard913 <Contact@saucedesigns.io>
2020-07-14 14:09:27 -0400

    Initial Commit

---
+++ DribbbleChallenge-Visa/DribbbleChallenge_VisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbbleChallenge_VisaApp.swift
+//  DribbbleChallenge-Visa
+//
+//  Created by AJ Picard on 7/14/20.
+//
+
+import SwiftUI
+
+@main
+struct DribbbleChallenge_VisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
